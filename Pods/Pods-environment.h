5bd20adb0a6362cb6a484031bdd98135362c2430
Joey Lorich <joey@cloudspace.com>
2013-08-02 13:09:26 -0400

    Initial commit

---
+++ Pods/Pods-environment.h
@@ -0,0 +1,63 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// AdMob
+#define COCOAPODS_POD_AVAILABLE_AdMob
+#define COCOAPODS_VERSION_MAJOR_AdMob 6
+#define COCOAPODS_VERSION_MINOR_AdMob 4
+#define COCOAPODS_VERSION_PATCH_AdMob 2
+
+// Appirater
+#define COCOAPODS_POD_AVAILABLE_Appirater
+#define COCOAPODS_VERSION_MAJOR_Appirater 1
+#define COCOAPODS_VERSION_MINOR_Appirater 0
+#define COCOAPODS_VERSION_PATCH_Appirater 5
+
+// CSEnhancedTableView
+#define COCOAPODS_POD_AVAILABLE_CSEnhancedTableView
+#define COCOAPODS_VERSION_MAJOR_CSEnhancedTableView 0
+#define COCOAPODS_VERSION_MINOR_CSEnhancedTableView 0
+#define COCOAPODS_VERSION_PATCH_CSEnhancedTableView 1
+
+// CSShortcuts
+#define COCOAPODS_POD_AVAILABLE_CSShortcuts
+#define COCOAPODS_VERSION_MAJOR_CSShortcuts 0
+#define COCOAPODS_VERSION_MINOR_CSShortcuts 0
+#define COCOAPODS_VERSION_PATCH_CSShortcuts 1
+
+// CSStandardViewControllers
+#define COCOAPODS_POD_AVAILABLE_CSStandardViewControllers
+#define COCOAPODS_VERSION_MAJOR_CSStandardViewControllers 0
+#define COCOAPODS_VERSION_MINOR_CSStandardViewControllers 0
+#define COCOAPODS_VERSION_PATCH_CSStandardViewControllers 1
+
+// GPUImage@siuying
+#define COCOAPODS_POD_AVAILABLE_GPUImage_siuying
+#define COCOAPODS_VERSION_MAJOR_GPUImage_siuying 0
+#define COCOAPODS_VERSION_MINOR_GPUImage_siuying 0
+#define COCOAPODS_VERSION_PATCH_GPUImage_siuying 2
+
+// GoogleAnalytics-iOS-SDK
+#define COCOAPODS_POD_AVAILABLE_GoogleAnalytics_iOS_SDK
+// This library does not follow semantic-versioning,
+// so we were not able to define version macros.
+// Please contact the author.
+// Version: 2.0beta4.
+
+// MBProgressHUD
+#define COCOAPODS_POD_AVAILABLE_MBProgressHUD
+#define COCOAPODS_VERSION_MAJOR_MBProgressHUD 0
+#define COCOAPODS_VERSION_MINOR_MBProgressHUD 7
+#define COCOAPODS_VERSION_PATCH_MBProgressHUD 0
+
+// TestFlightSDK
+#define COCOAPODS_POD_AVAILABLE_TestFlightSDK
+#define COCOAPODS_VERSION_MAJOR_TestFlightSDK 1
+#define COCOAPODS_VERSION_MINOR_TestFlightSDK 3
+#define COCOAPODS_VERSION_PATCH_TestFlightSDK 0
+
